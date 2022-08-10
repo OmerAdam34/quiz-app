af371e65adb73031d00e3c6dd3fcf6ca8883737d
Ömer Algin <oemer-algin@hotmail.de>
2022-08-10 23:11:55 +0200

    add const in card.js and in index.js

--- js/card.js
+++ js/card.js
@@ -1,3 +1,8 @@
 export default function card(cardElement){
-    
+    const button = document.querySelector('[data-js="button"]');
+    const answer = document.querySelector('[data-js="answer"]')
+
+console.log(card.classList);
+
 }
+
--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 import card from "./card.js";
 
+const flexboxcard = document.querySelector('[data-js="flexbox-card"]');
+const footerCard = document.querySelector('[data-js="footer-card"]');
+const headerCard = document.querySelector('[data-js="header-card"]');
+
+// header-card
+// footer-card
+// flexbox-card
