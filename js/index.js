b0096a6eb1773797599527f03c3546a7c459b085
Ömer Algin <oemer-algin@hotmail.de>
2022-08-11 19:45:58 +0200

    quiz cards in js

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ const flexboxCard = document.querySelector('[data-js="flexbox-card"]');
 const footerCard = document.querySelector('[data-js="footer-card"]');
 const headerCard = document.querySelector('[data-js="header-card"]');
 
-console.log("moin");
-
 card(flexboxCard);
 card(footerCard);
 card(headerCard);
