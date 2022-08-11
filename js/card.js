815e66d0a95c6bbf2d1afc95a731c0ae0ec37386
Ömer Algin <oemer-algin@hotmail.de>
2022-08-11 09:29:40 +0200

    links

--- js/card.js
+++ js/card.js
@@ -1,8 +1,16 @@
 export default function card(cardElement){
-    const button = document.querySelector('[data-js="button"]');
-    const answer = document.querySelector('[data-js="answer"]')
+    const button = cardElement.querySelector('[data-js="button"]');
+    const answer = cardElement.querySelector('[data-js="answer"]');
 
-console.log(card.classList);
+console.log(answer.classList);
+button.addEventListener('click', () => {
+    answer.classList.toggle('hidden');
 
+    if (answer.classList.contains('hidden')) {
+        button.textContent = 'Show answer';
+    } else {
+        button.textContent = 'Hide answer';
+    }
+});
 }
 
--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 import card from "./card.js";
 
-const flexboxcard = document.querySelector('[data-js="flexbox-card"]');
+const flexboxCard = document.querySelector('[data-js="flexbox-card"]');
 const footerCard = document.querySelector('[data-js="footer-card"]');
 const headerCard = document.querySelector('[data-js="header-card"]');
 
-// header-card
-// footer-card
-// flexbox-card
+console.log("moin");
+
+card(flexboxCard);
+card(footerCard);
+card(headerCard);
+
