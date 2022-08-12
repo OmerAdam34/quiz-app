9adedb2afe4ed16e04c52f79fed88b7e040838f1
Ömer Algin <oemer-algin@hotmail.de>
2022-08-12 13:04:47 +0200

    hidden button

--- js/card.js
+++ js/card.js
@@ -1,16 +1,15 @@
-export default function card(cardElement){
-    const button = cardElement.querySelector('[data-js="button"]');
-    const answer = cardElement.querySelector('[data-js="answer"]');
+export default function card(cardElement) {
+  const button = cardElement.querySelector('[data-js="button"]');
+  const answer = cardElement.querySelector('[data-js="answer"]');
 
-console.log(answer.classList);
-button.addEventListener('click', () => {
-    answer.classList.toggle('hidden');
+  console.log(answer.classList);
+  button.addEventListener("click", () => {
+    answer.classList.toggle("hidden");
 
-    if (answer.classList.contains('hidden')) {
-        button.textContent = 'Show answer';
+    if (answer.classList.contains("hidden")) {
+      button.textContent = "Show answer";
     } else {
-        button.textContent = 'Hide answer';
+      button.textContent = "Hide answer";
     }
-});
+  });
 }
-
--- js/index.js
+++ js/index.js
@@ -3,8 +3,11 @@ import card from "./card.js";
 const flexboxCard = document.querySelector('[data-js="flexbox-card"]');
 const footerCard = document.querySelector('[data-js="footer-card"]');
 const headerCard = document.querySelector('[data-js="header-card"]');
+const testCard_1 = document.querySelector('[data-js="testCard"]');
+const testCard_2 = document.querySelector('[data-js="testCard2"]');
 
 card(flexboxCard);
 card(footerCard);
 card(headerCard);
-
+card(testCard_1);
+card(testCard_2);
