fec511e74ee9e3ba0f40742d50ee1e00e1de909b
Ömer Algin <oemer-algin@hotmail.de>
2022-08-16 11:09:20 +0200

    changing bookmarkbuttons color wirh js

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,13 +1,19 @@
 export default function bookmark() {
-  const btn = document.querySelector('[data-js="bookmarkBtn"]');
+  // const btn = document.querySelector('[data-js="bookmarkBtn"]');
 
-  btn.addEventListener("click", () => {
-    btn.classList.toggle;
+  // btn.addEventListener("click", () => {
+  //   btn.classList.toggle;
+  //   if (btn.classList.toggle("click")) {
+  //     btn.style.stroke = "#3c7f9c";
+  //   } else {
+  //     btn.style.stroke = "#303030";
+  //   }
+  // });
 
-    if (btn.classList.toggle("click")) {
-      btn.style.stroke = "#3c7f9c";
-    } else {
-      btn.style.stroke = "#303030";
-    }
+  const btns = document.querySelectorAll('[data-js="bookmarkBtn"]');
+  btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.classList.toggle("bookmarkButton--active");
+    });
   });
 }
--- js/index.js
+++ js/index.js
@@ -1,15 +1,13 @@
 import singlePage from "./bookmark.js";
 import card from "./card.js";
-import bookmark from "./single-page.js";
 import pageNames from "./page-names.js";
+import bookmark from "./single-page.js";
 
 const flexboxCard = document.querySelector('[data-js="flexbox-card"]');
 const footerCard = document.querySelector('[data-js="footer-card"]');
 const headerCard = document.querySelector('[data-js="header-card"]');
 const testCard_1 = document.querySelector('[data-js="testCard"]');
 const testCard_2 = document.querySelector('[data-js="testCard2"]');
-
-const buttons = document.querySelectorAll('[data-js="bookmarkBtn"]');
 
 card(flexboxCard);
 card(footerCard);
