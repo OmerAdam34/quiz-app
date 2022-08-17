d8a80f1c36bbd5b1223b8bbde0f995e2d6f727b8
Ömer Algin <oemer-algin@hotmail.de>
2022-08-17 21:31:16 +0200

    delete comments in js, css

--- js/bookmark.js
+++ js/bookmark.js
@@ -1,15 +1,4 @@
 export default function bookmark() {
-  // const btn = document.querySelector('[data-js="bookmarkBtn"]');
-
-  // btn.addEventListener("click", () => {
-  //   btn.classList.toggle;
-  //   if (btn.classList.toggle("click")) {
-  //     btn.style.stroke = "#3c7f9c";
-  //   } else {
-  //     btn.style.stroke = "#303030";
-  //   }
-  // });
-
   const btns = document.querySelectorAll('[data-js="bookmarkBtn"]');
   btns.forEach((btn) => {
     btn.addEventListener("click", () => {
